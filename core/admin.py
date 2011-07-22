483476fdc62b22b9dfe6675de694df04d0468c3b
Konstantin Mochalov <incredible.angst@gmail.com>
2011-07-23 00:29:42 +0400

    Versioning in admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from reversion.admin import VersionAdmin
 from models import RouteMaster, Route, RouteOsmStop, OsmStop, ListedStop
 
-class RouteMasterAdmin(admin.ModelAdmin):
+class RouteMasterAdmin(VersionAdmin):
+    pass
+
+class RouteAdmin(VersionAdmin):
     pass
 
 admin.site.register(RouteMaster, RouteMasterAdmin)
+admin.site.register(Route, RouteAdmin)
