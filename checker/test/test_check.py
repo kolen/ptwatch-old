3c6ef82675ddec80d742825de041d64825de38f0
Konstantin Mochalov <incredible.angst@gmail.com>
2011-08-10 01:34:24 +0400

    Check oneway error present in first dataset and fixed in another.

--- checker/test/test_check.py
+++ checker/test/test_check.py
@@ -5,9 +5,14 @@ import checker
 
 class TestCheck(TestCase):
     TEST_RESOURCES_DIR = "checker/test/resources"
-    def setUp(self):
-        self.data = load(self.TEST_RESOURCES_DIR+"/yoshkar-ola_1.osm")
 
     def testCheck1(self):
-        results = checker.Checker.check_route_variant(self.data[2][380499], [], "bus")
-        self.assertEquals(set(), results.errors)
+        data = load(self.TEST_RESOURCES_DIR+"/yoshkar-ola_1.osm")
+        results = checker.Checker.check_route_variant(data[2][380499], [], "bus")
+        self.assertEquals(["TOPO_ONEWAY_WRONG_DIRECTION"], results.errors.keys())
+        self.assertEquals([31746305], [e.id for e in results.errors["TOPO_ONEWAY_WRONG_DIRECTION"].wrong_direction_ways])
+
+    def testCheck1OnewayFixed(self):
+        data = load(self.TEST_RESOURCES_DIR+"/yoshkar-ola_1_oneway_fixed.osm")
+        results = checker.Checker.check_route_variant(data[2][380499], [], "bus")
+        self.assertEquals({}, results.errors)
